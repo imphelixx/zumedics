5a1f88be1fd22192113b6141f7cfe9cbf0009c5d
imphelixx <125856098+imphelixx@users.noreply.github.com>
2023-03-27 11:32:56 +0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,16 @@
+<section class="py-7">
+    <div class="container">
+        <h3 class="text-center fw-bolder">Welcome to Medical Certrificate Generator</h3>
+        <hr>
+        The Zetech University Medical Certificate Generator is a software that automates the process of generating medical certificates for students. This software aims to make the process of getting a medical certificate as seamless as possible. The software is designed to produce certificates that adhere to the standards and requirements of Zetech University.
+<br>
+<br>
+The software is easy to use and can be accessed by authorized personnel. It comes with a user-friendly interface that allows users to input student details, medical information, and other necessary details required in the medical certificate. The software also comes with pre-built templates that can be used to generate certificates quickly and efficiently.
+
+
+<br>
+<br>
+
+The Zetech University Medical Certificate Generator is designed to ensure that medical certificates are produced accurately and in a timely manner. The software uses secure storage and data management to ensure that all student information is kept confidential. This software is an essential tool for Zetech University and will help streamline the process of generating medical certificates for its students.
+    </div>
+</section>
